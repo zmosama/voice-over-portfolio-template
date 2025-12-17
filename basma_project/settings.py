e094b34dfd4b5e627268a754bd0567198b0437e5
mosama <email@mosama.me>
2025-12-17 22:10:44 +0200

    Fix: Rename DB to db_final.sqlite3 to fix mount

--- basma_project/settings.py
+++ basma_project/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'basma_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_final.sqlite3',
     }
 }
 
