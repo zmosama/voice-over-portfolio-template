67f76b5300cfb8d65d2f13f217cd447c2ac161c1
mosama <email@mosama.me>
2025-12-18 04:43:51 +0200

    Fix media serving in production

--- basma_project/urls.py
+++ basma_project/urls.py
@@ -11,3 +11,10 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    from django.urls import re_path
+    from django.views.static import serve
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
